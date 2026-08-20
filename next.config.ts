bc299f5bb1ee67db29333d299712ee172240b2c4
Lintung0 <linlin160309@gmail.com>
2026-08-20 23:34:39 +0700

    chore: allow wikimedia + youtube thumb host for umrah docs media

--- next.config.ts
+++ next.config.ts
@@ -48,6 +48,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
     ],
   },
 };
