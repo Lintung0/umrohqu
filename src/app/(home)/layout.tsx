a8eb76a2d15e33a096bcdf06a64bf916b4415545
Lintung0 <linlin160309@gmail.com>
2026-08-30 22:47:57 +0700

    feat: hapus floating bar compare di luar halaman compare

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
-import CompareFloatingBar from "@/components/shared/compare-floating-bar";
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -9,7 +8,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Navbar />
       <div>{children}</div>
       <Footer />
-      <CompareFloatingBar />
     </>
   );
 }
